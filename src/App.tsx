071d4dee80c9798c03e2a2dc6cbe2faf3cafa2da
Eric Thomas <bluebird6922@gmail.com>
2022-05-26 15:26:16 +0300

    route - components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,27 @@
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import DayPass from './components/DayPass';
+import Items from './components/Items';
+import Missions from './components/Missions';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path={'/about'}>
+          <About />
+        </Route>
+        <Route path={'/day-pass'}>
+          <DayPass />
+        </Route>
+        <Route path={'/items'}>
+          <Items />
+        </Route>
+        <Route path={'/missions'}>
+          <Missions />
+        </Route>
         <Route path={'/'}>
           <Home />
         </Route>
