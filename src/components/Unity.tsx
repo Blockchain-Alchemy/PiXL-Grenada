22b3c6a1c2eab895cbdcd691af167bfb5587fff1
Eric Thomas <bluebird6922@gmail.com>
2022-05-27 11:27:11 +0300

    unity component

---
+++ src/components/Unity.tsx
@@ -0,0 +1,37 @@
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import "../../App.css";
+import Unity, { UnityContext } from "react-unity-webgl";
+import { TezosToolkit } from "@taquito/taquito";
+import { Tzip12Module } from "@taquito/tzip12";
+import { Tzip16Module } from "@taquito/tzip16";
+// import { MintProvider } from "../../services/mintProvider";
+// import { QuestProvider } from "../../services/questProvider";
+// import { GraveyardProvider } from "../../services/graveyardProvider";
+// import toast, { Toaster } from "react-hot-toast";
+// import LoadingComponent from "./loading";
+// import Items from "./Items";
+// import HelpMessage from "./HelpMessage";
+// import EntryCoin from "./EntryCoin";
+// import LoadingBar from "./LoadingBar";
+// import { CommonProvider, TokenInfo } from "../../services/common";
+
+export type ItemType = {
+  name: string;
+  imageSrc: string;
+  alt: string;
+  id?: number;
+  unityCardIdentifier?: number;
+};
+
+const unityContext = new UnityContext({
+  loaderUrl: "Build/1.loader.js",
+  dataUrl: "Build/1.data",
+  frameworkUrl: "Build/1.framework.js",
+  codeUrl: "Build/1.wasm",
+});
+
+const UnityComponent = () => {
+  
+};
+
+export default UnityComponent;
