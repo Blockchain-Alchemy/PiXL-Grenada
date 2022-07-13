ef1c57064b27da96b7f2cbb87e5d0a329646e5b3
Eric Thomas <bluebird6922@gmail.com>
2022-07-13 14:10:52 +0300

    day pass token

--- src/hooks/useDayPass.ts
+++ src/hooks/useDayPass.ts
@@ -1,55 +1,58 @@
-import { Contracts, DayPassToken } from "config";
-import {useCallback} from "react";
-import moment from "moment";
-import { useTezosContext } from "./useTezosContext";
+import { useCallback } from 'react';
+import moment from 'moment';
+import { Contracts, DayPassToken } from 'config';
+import { useTezosContext } from './useTezosContext';
 
 const contractAddress = Contracts.DayPass;
 
 const useDayPass = () => {
   const { tezos, walletAddress } = useTezosContext()!;
 
-  const mintToken = useCallback((tokenId: DayPassToken) => {
-    console.log('mintToken', tokenId);
-    if (tezos && walletAddress) {
-      const params = [{
-        amount: 1,
-        to_: walletAddress,
-        token: {
-          existing: tokenId,
-        }
-      }]
-      return tezos.wallet.at(contractAddress)
-        .then(contract => {
-          return contract.methods.mint(params).send();
-        })
-        .then(op => op.confirmation())
-        .then(result => {
-          console.log("Mint result:", result)
-          return result;
-        })
-    } else {
-      return Promise.reject('Please connect your wallet');
-    }
-  }, [tezos, walletAddress]);
+  const mintToken = useCallback(
+    (tokenId: DayPassToken) => {
+      console.log('mintToken', tokenId);
+      const mint = async () => {
+        const params = [
+          {
+            amount: 1,
+            to_: walletAddress,
+            token: {
+              existing: tokenId,
+            },
+          },
+        ];
+        const contract = await tezos.wallet.at(contractAddress);
+        const op = await contract.methods.mint(params).send();
+        const tx = await op.confirmation();
+        console.log('Mint result:', tx);
+        return tx;
+      };
+      return tezos && walletAddress
+        ? mint()
+        : Promise.reject('Please connect your wallet');
+    },
+    [tezos, walletAddress]
+  );
 
-  const getTokenTime = useCallback((tokenId: DayPassToken) => {
-    console.log('checkTokenTime', tokenId);
-    if (tezos && walletAddress) {
-      return tezos.wallet.at(contractAddress)
-        .then(contract => {
-          return contract.storage()
-        })
-        .then((storage: any) => {
-          return storage.token_times.get({ 0: walletAddress, 1: tokenId })
-        })
-        .then(token => {
-          console.log('token', token)
-          return token? moment(token) : null;
-        })
-    } else {
-      return Promise.reject('Please connect your wallet');
-    }
-  }, [tezos, walletAddress]);
+  const getTokenTime = useCallback(
+    (tokenId: DayPassToken) => {
+      console.log('checkTokenTime', tokenId);
+      const getToken = async () => {
+        const contract = await tezos.wallet.at(contractAddress);
+        const storage: any = await contract.storage();
+        const token = await storage.token_times.get({
+          0: walletAddress,
+          1: tokenId,
+        });
+        console.log('DayPass Token:', token);
+        return token ? moment(token) : null;
+      };
+      return tezos && walletAddress
+        ? getToken()
+        : Promise.reject('Please connect your wallet');
+    },
+    [tezos, walletAddress]
+  );
 
   return {
     mintToken,
